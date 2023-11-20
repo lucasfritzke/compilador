dd4707f2b369ef74babb5c45d454eccfa39e0116
Lucas Fritzke <86270203+lucasfritzke@users.noreply.github.com>
2023-11-20 19:56:40 -0300

    correcao gals

--- src/gals/Semantico.java
+++ src/gals/Semantico.java
@@ -4,6 +4,9 @@ package gals;
 
 public class Semantico implements Constants {
 
+
+    
+
     public String  executeAction(int action, Token token) throws SemanticError {
         System.out.println("A��o #" + action + ", Token: " + token);
         String str ="";
